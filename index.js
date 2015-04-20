439a4852c86d75c9c29391f5c0c350bed42f26c8
Maciej Małecki <me@mmalecki.com>
2015-04-19 22:13:03 -0700

    This party is a dependency of that other party so get this started too

---
+++ index.js
@@ -0,0 +1,26 @@
+var assert = require('assert')
+var child_process = require('child_process')
+var ExitError = require('exit-error')
+
+module.exports = function run(cmd, options, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = undefined
+  }
+
+  var opts = options || {}
+  var runner = opts.runner || child_process
+  var child = runner.spawn('/bin/sh', ['-c', cmd], options)
+
+  console.log('running:', cmd)
+
+  // TODO: make this output something fancier
+  if (child.stdout) child.stdout.pipe(process.stdout)
+  if (child.stderr) child.stderr.pipe(process.stderr)
+
+  child.on('close', function(code, signal) {
+    cb(ExitError(cmd, code, signal))
+  });
+
+  return child
+}
